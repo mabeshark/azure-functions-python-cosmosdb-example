d1faede3d36e609d24fc9614dba51dcd5a26e0c2
markmabe <markmabe@microsoft.com>
2021-04-30 11:22:57 -0400

    generate uuid test

--- backend/api/WritePatientInformation/__init__.py
+++ backend/api/WritePatientInformation/__init__.py
@@ -1,5 +1,5 @@
 import logging
-
+import uuid
 import azure.functions as func
 
 
@@ -15,10 +15,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    my_uuid = str(uuid.uuid4())
+
+    return func.HttpResponse(
+            f"This HTTP triggered function executed successfully. uuid={my_uuid} name={name}",
+            status_code=200
+    )
